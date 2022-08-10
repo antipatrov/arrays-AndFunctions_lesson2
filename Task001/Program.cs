2032a55442e51409b75a14f3a2eefcb68f301be4
Yuri_Antipatrov <89209578005@yandex.ru>
2022-08-10 14:50:43 +0300

    arrays and functions lesson2

---
+++ Task001/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите число: ");
+int threeDigit = Convert.ToInt32(Console.ReadLine());
+
+string stringNumber = Convert.ToString(threeDigit);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
