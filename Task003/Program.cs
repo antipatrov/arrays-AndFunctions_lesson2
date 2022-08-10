0c22c1f8d8795b41d90ac9be5fb517c65935e67a
Yuri_Antipatrov <89209578005@yandex.ru>
2022-08-10 16:52:06 +0300

    arrays and functions Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Введите номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if (dayNumber > 5)
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("рабочий");
+}
